df3bb285767fc720a485a403fdad1a7feca4802e
Prashuna Khanal <prashunakhanal8@gmail.com>
2026-07-05 14:12:36 +0545

    Round Robin Scheduling implemented

--- Task1.c
+++ Task1.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/utsname.h>
+#include <string.h>
 
 // Shared resource
 int counter = 0;
@@ -9,7 +10,14 @@ int counter = 0;
 // Mutex for synchronization
 pthread_mutex_t mutex;
 
-// System information thread
+// Structure for Round Robin processes
+typedef struct
+{
+    char name[10];
+    int burstTime;
+} Process;
+
+// System Information Thread
 void *systemInfo(void *arg)
 {
     struct utsname sysInfo;
@@ -22,7 +30,7 @@ void *systemInfo(void *arg)
     return NULL;
 }
 
-// Simulates a disk read operation
+// Disk I/O Thread
 void *diskIO(void *arg)
 {
     for (int i = 0; i < 5; i++)
@@ -42,7 +50,7 @@ void *diskIO(void *arg)
     return NULL;
 }
 
-// Simulates a user process
+// User Process Thread
 void *userProcess(void *arg)
 {
     for (int i = 0; i < 5; i++)
@@ -62,6 +70,53 @@ void *userProcess(void *arg)
     return NULL;
 }
 
+// Round Robin Scheduler
+void roundRobin(Process processes[], int n, int quantum)
+{
+    int remaining[n];
+
+    // Copy burst times
+    for (int i = 0; i < n; i++)
+    {
+        remaining[i] = processes[i].burstTime;
+    }
+
+    printf("Round Robin Scheduling\n");
+    printf("Time Quantum = %d\n\n", quantum);
+
+    int done;
+
+    do
+    {done = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (remaining[i] > 0)
+            {
+                done = 0;
+
+                if (remaining[i] > quantum)
+                {
+                    printf("%s runs for %d units\n",
+                           processes[i].name,
+                           quantum);
+
+                    remaining[i] -= quantum;
+                }
+                else
+                {
+                    printf("%s runs for %d units and finishes\n",
+                           processes[i].name,
+                           remaining[i]);
+
+                    remaining[i] = 0;
+                }
+            }
+        }
+
+    } while (!done);
+}
+
+// Main Function
 int main()
 {
     pthread_t thread1, thread2, thread3;
@@ -71,16 +126,28 @@ int main()
 
     printf("Main Process ID: %d\n\n", getpid());
 
+    // Create threads
     pthread_create(&thread1, NULL, systemInfo, NULL);
     pthread_create(&thread2, NULL, diskIO, NULL);
     pthread_create(&thread3, NULL, userProcess, NULL);
 
+    // Wait for threads to finish
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
     pthread_join(thread3, NULL);
 
     printf("\nExpected Counter = 10\n");
     printf("Final Counter = %d\n", counter);
+
+    // Round Robin Simulation
+    Process processes[] =
+    {
+        {"P1", 5},
+        {"P2", 3},
+        {"P3", 4}
+    };
+
+    roundRobin(processes, 3, 2);
 
     // Destroy mutex
     pthread_mutex_destroy(&mutex);
