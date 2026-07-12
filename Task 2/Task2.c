46b89c73d50e04978d665248c92da4098a2bc482
Prashuna Khanal <prashunakhanal8@gmail.com>
2026-07-12 12:44:14 +0545

    Add page size and frame configuration

--- Task 2/Task2.c
+++ Task 2/Task2.c
@@ -1,8 +1,24 @@
+# Memory Management Simulation
 #include <stdio.h>
+#define size 10
 
+int pages[size] = {1,2,3,2,4,1,5,2,1,2};
 int main()
-{
+{   int pageSize;
+    int frameSize;
+    int i;
     printf("Memory Management Simulator\n");
+    printf("Enter Page Size (KB): ");
+    scanf("%d",&pageSize);
+    
+    printf("Enter Number of Frames: ");
+    scanf("%d",&frameSize);
 
+    printf("\nPage Size = %d KB\n",pageSize);
+    printf("Reference String:\n");
+
+    for(i=0;i<size;i++)
+        printf("%d ",pages[i]);
+    printf("\n");
     return 0;
 }
