a5d0a05c12633c1c99e6de20b0009ebfa5dfb9c2
Prashuna Khanal <prashunakhanal8@gmail.com>
2026-07-12 12:51:10 +0545

    Implement FIFO page replacement algorithm

--- Task 2/Task2.c
+++ Task 2/Task2.c
@@ -1,8 +1,35 @@
-# Memory Management Simulation
+//Memory Management Simulation
 #include <stdio.h>
 #define size 10
 
 int pages[size] = {1,2,3,2,4,1,5,2,1,2};
+
+void FIFO(int frameSize)
+{
+    int frame[10];
+    int i,j;
+    int index=0;
+    for(i=0;i<frameSize;i++)
+        frame[i]=-1;
+
+    printf("\nFIFO Simulation\n");
+
+    for(i=0;i<size;i++)
+    {
+        int found=0;
+        for(j=0;j<frameSize;j++){
+            if(frame[j]==pages[i]){
+                found=1;
+                break;}}
+        if(found){
+            printf("Page %d -> HIT\n",pages[i]);}
+        else{
+            printf("Page %d -> FAULT\n",pages[i]);
+            frame[index]=pages[i];
+            index=(index+1)%frameSize;
+        }
+    }
+}
 int main()
 {   int pageSize;
     int frameSize;
@@ -20,5 +47,6 @@ int main()
     for(i=0;i<size;i++)
         printf("%d ",pages[i]);
     printf("\n");
+    FIFO(frameSize);
     return 0;
 }
