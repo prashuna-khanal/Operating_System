954ecab0942cacabecbe78e8b1b535b0d930be7d
Prashuna Khanal <prashunakhanal8@gmail.com>
2026-07-20 16:49:44 +0545

    Scenario added for the task

--- Task 3/Task3.c
+++ Task 3/Task3.c
@@ -4,29 +4,50 @@ int main() {
     int choice;
 
     do {
-        printf("\n Secure File Management System \n");
+        printf(" Student Assignment File Management\n");
         printf("1. Login\n");
-        printf("2. Create File\n");
-        printf("3. Read File\n");
-        printf("4. Write File\n");
-        printf("5. Delete File\n");
-        printf("6. Set Permissions\n");
-        printf("7. View Permissions\n");
+        printf("2. Create Assignment File\n");
+        printf("3. Read Assignment File\n");
+        printf("4. Write Assignment File\n");
+        printf("5. Delete Assignment File\n");
+        printf("6. Manage File Permissions\n");
+        printf("7. View File Permissions\n");
         printf("8. Logout\n");
         printf("9. Exit\n");
 
-        printf("\nEnter choice: ");
+        printf("\nEnter your choice: ");
         scanf("%d", &choice);
 
         switch(choice) {
             case 1:
-                printf("Login.\n");
+                printf("Login feature coming soon.\n");
+                break;
+            case 2:
+                printf("Create File feature coming soon.\n");
+                break;
+            case 3:
+                printf("Read File feature coming soon.\n");
+                break;
+            case 4:
+                printf("Write File feature coming soon.\n");
+                break;
+            case 5:
+                printf("Delete File feature coming soon.\n");
+                break;
+            case 6:
+                printf("Permission feature coming soon.\n");
+                break;
+            case 7:
+                printf("View Permission feature coming soon.\n");
+                break;
+            case 8:
+                printf("Logout feature coming soon.\n");
                 break;
             case 9:
-                printf("Thankyou.The END\n");
+                printf("Exiting System...\n");
                 break;
             default:
-                printf("....\n");
+                printf("Invalid Choice!\n");
         }
 
     } while(choice != 9);
