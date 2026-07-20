2f0f2881cba3df9d68edb88af513b5e379a154c9
Prashuna Khanal <prashunakhanal8@gmail.com>
2026-07-20 21:30:48 +0545

    Seperate portal choices for lecturer and student along with login attempts

--- Task 3/Task3.c
+++ Task 3/Task3.c
@@ -1,87 +1,137 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-//Login Details
+int loggedIn = 0;
+char role[20];
+//LOGIN
 void login() {
     char username[20];
     char password[20];
-    printf("\nUsername: ");
-    scanf("%s", username);
-    printf("Password: ");
-    scanf("%s", password);
-    //strcmp is used for comparison of each characters
-    if(strcmp(username, "lecturer") == 0 && strcmp(password, "lec123") == 0) {
-        loggedIn = 1;
-        strcpy(role, "Lecturer");
-        printf("\nLogin Successful!\n");
-        printf("Welcome Lecturer\n");
-    }
-    else if(strcmp(username, "student") == 0 && strcmp(password, "stu123") == 0) {
-        loggedIn = 1;
-        strcpy(role, "Student");
-        printf("\nLogin Successful!\n");
-        printf("Welcome Student\n");
-    }
-    else {
-        printf("\nInvalid Username or Password\n");
-    }
-}
+    int attempts = 0;
 
+    while (attempts < 3) {
+        printf("\nUsername: ");
+        scanf("%s", username);
+        printf("Password: ");
+        scanf("%s", password);
+        if (strcmp(username, "lecturer") == 0 && strcmp(password, "lec123") == 0) {
+            loggedIn = 1;
+            strcpy(role, "Lecturer");
+            printf("\nLogin Successful!\n");
+            printf("Welcome to the portal !!\n");
+            return;
+        }
+        else if (strcmp(username, "student") == 0 && strcmp(password, "stu123") == 0) {
+            loggedIn = 1;
+            strcpy(role, "Student");
+            printf("\nLogin Successful!\n");
+            printf("Welcome Student\n");
+            return;
+        }
+        else {
+            attempts++;
+            printf("\nInvalid Username or Password.\n");
+            if (attempts < 3)
+                printf("Attempts Remaining: %d\n", 3 - attempts);
+        }
+    }
+    printf("\nToo many failed login attempts.\n");
+    exit(0);
+}
 int main() {
     int choice;
-    int loggedIn = 0;
-    char role[20];
-
-    do {
-        printf(" Student Assignment File Management\n");
-        printf("1. Login\n");
-        printf("2. Create Assignment File\n");
-        printf("3. Read Assignment File\n");
-        printf("4. Write Assignment File\n");
-        printf("5. Delete Assignment File\n");
-        printf("6. Manage File Permissions\n");
-        printf("7. View File Permissions\n");
-        printf("8. Logout\n");
-        printf("9. Exit\n");
-
-        printf("\nEnter your choice: ");
-        scanf("%d", &choice);
-
-        switch(choice) {
-            case 1:
-                login();
-                break;
-            case 2:
-                printf("Create File feature coming soon.\n");
-                break;
-            case 3:
-                printf("Read File feature coming soon.\n");
-                break;
-            case 4:
-                printf("Write File feature coming soon.\n");
-                break;
-            case 5:
-                printf("Delete File feature coming soon.\n");
-                break;
-            case 6:
-                printf("Permission feature coming soon.\n");
-                break;
-            case 7:
-                printf("View Permission feature coming soon.\n");
-                break;
-            case 8:
-                loggedIn = 0;
-                strcpy(role, "");
-                printf("Logged Out Successfully.\n");
-                break;
-            case 9:
-                printf("Exiting System...\n");
-                break;
-            default:
-                printf("Invalid Choice!\n");
+    while (1) {
+        // LOGIN MENU
+        if (!loggedIn) {
+            printf(" Student Assignment File Management\n");
+            printf("1. Login\n");
+            printf("2. Exit\n");
+            printf("\nEnter your choice: ");
+            scanf("%d", &choice);
+            switch(choice){
+                case 1:
+                    login();
+                    break;
+                case 2:
+                    printf("Thank You!\n");
+                    return 0;
+                default:
+                    printf("Invalid Choice!\n");
+            }
         }
-
-    } while(choice != 9);
-
+        //LECTURER MENU
+        else if(strcmp(role,"Lecturer")==0){
+            printf("\n Lecturer Menu \n");
+            printf("1. Create Assignment File\n");
+            printf("2. Read Assignment File\n");
+            printf("3. Write Assignment File\n");
+            printf("4. Delete Assignment File\n");
+            printf("5. Manage File Permissions\n");
+            printf("6. View File Permissions\n");
+            printf("7. Logout\n");
+            printf("8. Exit\n");
+            printf("\nEnter your choice: ");
+            scanf("%d",&choice);
+            switch(choice){
+                case 1:
+                    printf("Create Assignment File feature coming soon.\n");
+                    break;
+                case 2:
+                    printf("Read Assignment File feature coming soon.\n");
+                    break;
+                case 3:
+                    printf("Write Assignment File feature coming soon.\n");
+                    break;
+                case 4:
+                    printf("Delete Assignment File feature coming soon.\n");
+                    break;
+                case 5:
+                    printf("Manage File Permissions feature coming soon.\n");
+                    break;
+                case 6:
+                    printf("View File Permissions feature coming soon.\n");
+                    break;
+                case 7:
+                    loggedIn = 0;
+                    strcpy(role,"");
+                    printf("Logged Out Successfully.\n");
+                    break;
+                case 8:
+                    printf("Thank You!\n");
+                    return 0;
+                default:
+                    printf("Invalid Choice!\n");
+            }
+        }
+        //STUDENT MENU 
+        else{
+            printf("\n Student Menu \n");
+            printf("1. Read Assignment File\n");
+            printf("2. View File Permissions\n");
+            printf("3. Logout\n");
+            printf("4. Exit\n");
+            printf("\nEnter your choice: ");
+            scanf("%d",&choice);
+            switch(choice){
+                case 1:
+                    printf("Read Assignment File feature coming soon.\n");
+                    break;
+                case 2:
+                    printf("View File Permissions feature coming soon.\n");
+                    break;
+                case 3:
+                    loggedIn = 0;
+                    strcpy(role,"");
+                    printf("Logged Out Successfully.\n");
+                    break;
+                case 4:
+                    printf("Thank You!\n");
+                    return 0;
+                default:
+                    printf("Invalid Choice!\n");
+            }
+        }
+    }
     return 0;
 }
